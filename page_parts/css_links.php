a34701a897c77220ca639889e81b9e209a6da7d3
Pedro Mass <drom296@gmail.com>
2011-11-05 21:38:41 -0400

    Modularized the CSS.

---
+++ page_parts/css_links.php
@@ -0,0 +1,5 @@
+	<link type="text/css" rel="stylesheet" href="css/nav.css" />
+	<link type="text/css" rel="stylesheet" href="css/knox_nav.css" />
+	<link type="text/css" rel="stylesheet" href="css/knox.css" />
+	<link type="text/css" rel="stylesheet" href="css/knox_color.css" />
+	<link type="text/css" rel="stylesheet" href="css/content.css" />
